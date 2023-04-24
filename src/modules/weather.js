2ab96e471d79f91f562547abde4ec7d5130cd964
Ola <ohagenbendixen@gmail.com>
2023-04-24 17:15:52 +0200

    Create a errorHandler func

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,13 +1,23 @@
 const weather = (() => {
+  function errorHandler(err) {
+    if (err.error.message) {
+      console.log(err.error.message);
+    } else {
+      console.log(err);
+    }
+  }
+
   async function getWeather(place) {
     try {
       const weatherResponse = await fetch(
         `https://api.weatherapi.com/v1/current.json?key=7effff997da04f9595863506232004&q=${place}`,
       );
       const weatherData = await weatherResponse.json();
-      console.log(weatherData);
+      if (weatherData.error) {
+        errorHandler(weatherData);
+      }
     } catch (err) {
-      console.log(err);
+      errorHandler(err);
     }
   }
 

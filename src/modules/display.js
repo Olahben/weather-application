1cb121c3be4f47fca3b1ca637d7e21c9f468be75
Ola <ohagenbendixen@gmail.com>
2023-04-25 07:25:16 +0200

    Display weather info to the console

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,1 +1,12 @@
-import weather from './weather';
+const display = (() => {
+  function displayWeather(infoObj) {
+    const weatherInfoArray = Object.entries(infoObj);
+    weatherInfoArray.forEach((item) => {
+      console.log(item[1]);
+    });
+  }
+
+  return { displayWeather };
+})();
+
+export default display;

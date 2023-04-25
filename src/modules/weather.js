56093dac94d2dea743b3b3f45bc05b696d4e5f7c
Ola <ohagenbendixen@gmail.com>
2023-04-25 07:01:17 +0200

    Store the information about the weather in an obj

--- src/modules/weather.js
+++ src/modules/weather.js
@@ -13,9 +13,21 @@ const weather = (() => {
         `https://api.weatherapi.com/v1/current.json?key=7effff997da04f9595863506232004&q=${place}`,
       );
       const weatherData = await weatherResponse.json();
+      console.log(weatherData);
       if (weatherData.error) {
         errorHandler(weatherData);
       }
+      const weatherInfo = {
+        country: await weatherData.location.country,
+        city: await weatherData.location.name,
+        condition: await weatherData.current.condition.text,
+        feelsLikeC: await weatherData.current.feelslike_c,
+        feelsLikeF: await weatherData.current.feelslike_f,
+        lastUpdated: await weatherData.current.last_updated,
+        tempC: await weatherData.current.temp_c,
+        tempF: await weatherData.current.temp_f,
+        windKPH: await weatherData.current.wind_kph,
+      };
     } catch (err) {
       errorHandler(err);
     }

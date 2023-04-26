96d5b6d548e4062116987dfdb468f2f938a5e673
Ola <ohagenbendixen@gmail.com>
2023-04-26 07:21:43 +0200

    Display weeather Info

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,9 +1,15 @@
 const display = (() => {
   function displayWeather(infoObj) {
-    const weatherInfoArray = Object.entries(infoObj);
-    weatherInfoArray.forEach((item) => {
-      console.log(item[1]);
-    });
+    const location = document.querySelector('h2');
+    location.textContent += `${infoObj.country}, ${infoObj.city}`;
+    const condition = document.querySelector('.condition');
+    condition.textContent += `${infoObj.condition}`;
+    const feelsLikeTemp = document.querySelector('.feels-like');
+    feelsLikeTemp.textContent += `${infoObj.feelsLikeC}C°`;
+    const lastUpdated = document.querySelector('.last-updated');
+    lastUpdated.textContent += `${infoObj.lastUpdated}`;
+    const temp = document.querySelector('.temp');
+    temp.textContent += `${infoObj.tempC}C°`;
   }
 
   return { displayWeather };
